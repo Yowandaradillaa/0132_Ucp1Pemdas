2637cd15f810a69ee2047370435ee9571f601770
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-04-29 10:30:34 +0700

    Selesai

--- UCP1Procedural_PemrogramanDasar/UCP1Procedural_PemrogramanDasar.cpp
+++ UCP1Procedural_PemrogramanDasar/UCP1Procedural_PemrogramanDasar.cpp
@@ -1,3 +1,74 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+	int a[75];
+	float indeks, nilai, jumlah, rerata;
+	string status;
+	string kota;
+
+	cout << "Masukan indeks tingkat polutan = ";
+	cin >> indeks;
+	cout << "Masukan nilai sensor  = ";
+	cin >> nilai;
+
+	rerata = (indeks+nilai) / 2;
+
+	if (rerata >= 75) {
+		status = "Sehat jalur rerata";
+	}
+	else if (indeks <= 75) {
+		status = "Tidak sehat karena tingkat polutan yang tinggi";
+	}
+	else
+	{
+		status = "Tidak sehat";
+	}
+
+	cout << "Statusnya adalah " << status << endl;
+
+	cout << "Masukan Nama kota :  ";
+	cin >> kota;
+	cout << "Masukan status sebenarnya :  ";
+	cin >> status;
+
+	cout << "Total lokasi dengan kualitas udara Sehat: " << status << endl;
+	cout << "Total lokasi dengan kualitas udara Tidak Sehat: " << status << endl;
+}
+
+//1. int a[75]
+// float indeks, jumlah, rerata
+// string status
+//2. - prosedur : void input {
+//cout << "Bilangan Pertama = ";
+//cin >> a;
+//cout << "Bilangan Kedua = ";
+//cin >> b;
+//}
+// - fungsi : 
+// int Penjumlahan(int bil_1, int bil_2) {
+//return bil_1 + bil_2;
+//}
+//3. for (i = 0; i < 6; i++) {
+//cout << i << ". " << "Nama saya adalah Yowanda" << endl;
+//}
+//4. rerata = jumlah / data;
+//if (rerata <= 75) {
+	//status = "Sehat jalur rerata";
+//}
+//else if (polutan >= 75) {
+	//status = "Tidak sehat karena tingkat polutan yang tinggi";
+//}
+//else
+//{
+	//status = "Tidak sehat";
+//}
+//cout << "Statusnya adalah " << status << endl;
+//5. struct dokter {
+//string nip;
+//string nama;
+//string jenis kelamin;
+//int umur;
+//};
+
+
